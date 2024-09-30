c27ac33674a9adaffd12dcc404d6eb1c3b867931
Ethan McDonald <118023721+emcodnald@users.noreply.github.com>
2024-09-30 15:38:36 -0400

    Update imageRender.py

--- imageRender.py
+++ imageRender.py
@@ -3,6 +3,8 @@ image rendering program
 takes a sequence of parametric curve and prism data and renders a bitmap image
 """
 import parametric
+import bitmap
+import jpg
 import math
 
 # example bezier curve
@@ -22,12 +24,13 @@ where the curve data is stored
 lines = [
     [1,[71,100],[[112,81]],[166,92],1,[0,0,0]],
     [1,[166,92],[[166,135]],[129,172],1,[0,0,0]],
-    [1,[129,172],[[119,131]],[71,100],1,[0,0,0]],
+    [1,[129,172],[[119,131]],[71,90],1,[0,0,0]],
+    [1,[166,92],[[224,248]],[54,253],1,[0,0,0]],
     ]
 
 # where the prism data is stored
 shapes = [
-    [[0,1,2],[128,128,128]]
+    [[0,3,2,1],[128,128,128]]
     ]
 
 # basic image info
@@ -46,7 +49,9 @@ for i in range(len(shapes)):
     cl = []
     for j in range(len(shapes[i][0])):
         cl.append(curves[shapes[i][0][j]])
-    prisms.append(parametric.prism(cl))
+    sub = parametric.prism(cl)
+    if sub.isPrism:
+        prisms.append(sub)
 
 # definition pixel matrix that represents each pixel in the image
 pixels = []
@@ -62,23 +67,22 @@ for i in range(len(prisms)):
         for k in range(prismSpace[j].center.y,prismSpace[j].center.y+prismSpace[j].size+1):
             for m in range(prismSpace[j].center.x,prismSpace[j].center.x+prismSpace[j].size+1):
                 for n in range(3):
-                    pixels[k][m][n] = shapes[i][1][n]
+                    pixels[dimensions[1]-k-1][m][n] = shapes[i][1][n]
 
 # draws all curves, done second so that outlines are on top
 for i in range(len(curves)):
     curvePixels = curves[i].generatePixels(lines[i][-2])
     for j in range(len(curvePixels)):
-        newPixelColor = pixels[curvePixels[j].pos.y][curvePixels[j].pos.x]
+        newPixelColor = pixels[dimensions[1]-curvePixels[j].pos.y-1][curvePixels[j].pos.x]
         for k in range(3):
             newPixelColor[k] = int(newPixelColor[k]*curvePixels[j].magnitude+lines[i][-1][k]*(1-curvePixels[j].magnitude))
-        pixels[curvePixels[j].pos.y][curvePixels[j].pos.x] = newPixelColor
+        pixels[dimensions[1]-curvePixels[j].pos.y-1][curvePixels[j].pos.x] = newPixelColor
 
-# writing the final image file in .bmp format
-startBytes = [66, 77, 110, 87, 7, 0, 0, 0, 0, 0, 54, 0, 0, 0, 40, 0, 0, 0, dimensions[0]%256, int(dimensions[0]/256)%256, int(dimensions[0]/65536)%256, int(dimensions[0]/16777216)%256, dimensions[1]%256, int(dimensions[1]/256)%256, int(dimensions[1]/65536)%256, int(dimensions[1]/16777216)%256, 1, 0, 24, 0, 0, 0, 0, 0, 56, 87, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-with open("result.bmp", 'wb') as f:
-    for i in range(len(startBytes)):
-        f.write(startBytes[i].to_bytes(1,byteorder='big'))
-    for i in range(len(pixels)):
-        for j in range(len(pixels[i])):
-            for k in range(len(pixels[i][j])):
-                f.write(pixels[i][j][2-k].to_bytes(1,byteorder='big'))
+# writing the final image file in selected format
+resFile = "result"
+form = ".jpg"
+if form == ".bmp":
+    bitmap.image(pixels, resFile+form)
+elif form == ".jpg":
+    jpg.image(pixels, resFile+form, 255)
+
