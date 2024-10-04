41e8580cd4ddb640882cdcae65652db5e86f26da
Ethan McDonald <118023721+emcodnald@users.noreply.github.com>
2024-10-03 23:37:00 -0400

    Update imageRender.py

--- imageRender.py
+++ imageRender.py
@@ -8,6 +8,7 @@ import parametric
 import bitmap
 import jpg
 import math
+import png
 
 """
 where the auxiliary data is stored
@@ -23,77 +24,23 @@ where the auxiliary data is stored
 9 = quad warp
 10 = bSpline curve
 """
-aux = []
 
-auxCurves = []
-for i in range(len(aux)):
-    if aux[i][0] == 1:
-        sub = []
-        for i in range(len(aux[i][2])):
-            sub.append(parametric.orderedPair(aux[i][2][0],aux[i][2][1]))
-        auxCurves.append(parametric.bez(parametric.orderedPair(aux[i][1][0],aux[i][1][1]),sub,parametric.orderedPair(aux[i][3][0],aux[i][3][1])))
-    elif aux[i][0] == 2:
-        auxCurves.append(parametric.spiral(aux[i][1],aux[i][2],aux[i][3],aux[i][4],aux[i][5],aux[i][6]))
-    elif aux[i][0] == 3:
-        sub = []
-        for j in range(len(aux[i][1])):
-            sub.append(auxCurves[aux[i][1][j]])
-        sub2 = []
-        for j in range(len(aux[i][2])):
-            sub2.append(parametric.transformationNode(aux[i][2][j]))
-        sub3 = parametric.transformCurve(sub,sub2)
-        for j in range(len(sub3)):
-            auxCurves.append(sub3[j])
-    elif aux[i][0] == 4:
-        auxCurves.append(parametric.para(auxCurves[aux[i][1]].x,auxCurves[aux[i][2]].y))
-    elif aux[i][0] == 5:
-        if aux[i][3]:
-            auxCurves.append(parametric.wrap(auxCurves[aux[i][1]],auxCurves[aux[i][2]].y,parametric.quickTransform(aux[i][4][0],aux[i][4][1])))
-        else:
-            auxCurves.append(parametric.wrap(auxCurves[aux[i][1]],auxCurves[aux[i][2]].x,parametric.quickTransform(aux[i][4][0],aux[i][4][1])))
-    elif aux[i][0] == 6:
-        auxCurves.append(parametric.tangent(auxCurves[aux[i][1]],aux[i][2],auxCurves[aux[i][3]],aux[i][4]))
-    elif aux[i][0] == 7:
-        sub = []
-        for j in range(len(aux[i][2])):
-            sub.append(auxCurves[aux[i][2][j]])
-        sub2 = parametric.singleWarp(auxCurves[aux[i][1]],sub,parametric.scopeDimensions(aux[i][3][0],aux[i][3][1]),parametric.orderedPair(aux[i][4][0],aux[i][4][1]))
-        for j in range(len(sub2)):
-            auxCurves.append(sub2[j])
-    elif aux[i][0] == 8:
-        sub = []
-        for j in range(len(aux[i][3])):
-            sub.append(auxCurves[aux[i][3][j]])
-        sub2 = parametric.doubleWarp(auxCurves[aux[i][1]],auxCurves[aux[i][2]],sub,parametric.scopeDimensions(aux[i][4][0],aux[i][4][1]),parametric.orderedPair(aux[i][5][0],aux[i][5][1]))
-        for j in range(len(sub2)):
-            auxCurves.append(sub2[j])
-    elif aux[i][0] == 9:
-        sub = []
-        for j in range(len(aux[i][5])):
-            sub.append(auxCurves[aux[i][5][j]])
-        sub2 = parametric.quadWarp(auxCurves[aux[i][1]],auxCurves[aux[i][2]],auxCurves[aux[i][3]],auxCurves[aux[i][4]],sub,parametric.scopeDimensions(aux[i][6][0],aux[i][6][1]),parametric.orderedPair(aux[i][7][0],aux[i][7][1]))
-        for j in range(len(sub2)):
-            auxCurves.append(sub2[j])
-    elif aux[i][0] == 10:
-        sub = []
-        for j in range(len(aux[i][2])):
-            sub.append(parametric.orderedPair(aux[i][2][j][0],aux[i][2][j][1]))
-        sub2 = parametric.bSpline(parametric.orderedPair(aux[i][1][0],aux[i][1][1]),sub,parametric.orderedPair(aux[i][3][0],aux[i][3][1]),aux[i][4][0],aux[i][4][1])
-        for j in range(len(sub2)):
-            curves.append(sub2[j])
-
+resFile = "result"
+form = ".png"
 """
 where the curve data is stored
 """
-sub = []
-for i in range(4,len(aux)):
-    sub.append(i)
 lines = [
-    [10,[60,30],[[120,30],[150,90],[90,120]],[30,90],[3,1],1,[0,0,0]]
+    [1,[71,100],[[112,81]],[166,92],True,1,[0,0,0,1]],
+    [1,[166,92],[[166,135]],[129,172],True,1,[0,0,0,1]],
+    [1,[129,172],[[119,131]],[71,100],True,1,[0,0,0,1]],
+    [1,[285,123],[[161,109]],[93,167],True,1,[0,0,0,1]],
+    [1,[285,123],[[78,65]],[93,167],True,1,[0,0,0,1]]
     ]
 # where the prism data is stored
 shapes = [
-    #[[0,3,2,1],[128,128,128]]
+    [[0,1,2],[255,0,0,0.5]],
+    [[3,4],[0,255,255,0.5]],
     ]
 
 # basic image info
@@ -108,64 +55,73 @@ for i in range(len(lines)):
         for j in range(len(lines[i][2])):
             sub.append(parametric.orderedPair(lines[i][2][j][0],lines[i][2][j][1]))
         curves.append(parametric.bez(parametric.orderedPair(lines[i][1][0],lines[i][1][1]),sub,parametric.orderedPair(lines[i][3][0],lines[i][3][1])))
+        curves[-1].vis = lines[i][-3]
         curves[-1].th = lines[i][-2]
         curves[-1].col = lines[i][-1]
     elif lines[i][0] == 2:
         curves.append(parametric.spiral(lines[i][1],lines[i][2],lines[i][3],lines[i][4],lines[i][5],lines[i][6]))
+        curves[-1].vis = lines[i][-3]
         curves[-1].th = lines[i][-2]
         curves[-1].col = lines[i][-1]
     elif lines[i][0] == 3:
         sub = []
         for j in range(len(lines[i][1])):
-            sub.append(auxCurves[lines[i][1][j]])
+            sub.append(curves[lines[i][1][j]])
         sub2 = []
         for j in range(len(lines[i][2])):
             sub2.append(parametric.transformationNode(lines[i][2][j]))
         sub3 = parametric.transformCurve(sub,sub2)
         for j in range(len(sub3)):
             curves.append(sub3[j])
+            curves[-1].vis = lines[i][-3]
             curves[-1].th = lines[i][-2]
             curves[-1].col = lines[i][-1]
     elif lines[i][0] == 4:
-        curves.append(parametric.para(auxCurves[lines[i][1]].x,auxCurves[lines[i][2]].y))
+        curves.append(parametric.para(curves[lines[i][1]].x,curves[lines[i][2]].y))
+        curves[-1].vis = lines[i][-3]
         curves[-1].th = lines[i][-2]
         curves[-1].col = lines[i][-1]
     elif lines[i][0] == 5:
         if lines[i][3]:
-            curves.append(parametric.wrap(auxCurves[lines[i][1]],auxCurves[lines[i][2]].y,parametric.quickTransform(lines[i][4][0],lines[i][4][1])))
+            curves.append(parametric.wrap(curves[lines[i][1]],curves[lines[i][2]].y,parametric.quickTransform(lines[i][4][0],lines[i][4][1])))
         else:
-            curves.append(parametric.wrap(auxCurves[lines[i][1]],auxCurves[lines[i][2]].x,parametric.quickTransform(lines[i][4][0],lines[i][4][1])))
+            curves.append(parametric.wrap(curves[lines[i][1]],curves[lines[i][2]].x,parametric.quickTransform(lines[i][4][0],lines[i][4][1])))
+        curves[-1].vis = lines[i][-3]
         curves[-1].th = lines[i][-2]
         curves[-1].col = lines[i][-1]
     elif lines[i][0] == 6:
-        curves.append(parametric.tangent(auxCurves[lines[i][1]],lines[i][2],auxCurves[lines[i][3]],lines[i][4]))
+        curves.append(parametric.tangent(curves[lines[i][1]],lines[i][2],curves[lines[i][3]],lines[i][4]))
+        curves[-1].vis = lines[i][-3]
         curves[-1].th = lines[i][-2]
         curves[-1].col = lines[i][-1]
     elif lines[i][0] == 7:
         sub = []
         for j in range(len(lines[i][2])):
-            sub.append(auxCurves[lines[i][2][j]])
-        sub2 = parametric.singleWarp(auxCurves[lines[i][1]],sub,parametric.scopeDimensions(lines[i][3][0],lines[i][3][1]),parametric.orderedPair(lines[i][4][0],lines[i][4][1]))
+            sub.append(curves[lines[i][2][j]])
+        sub2 = parametric.singleWarp(curves[lines[i][1]],sub,parametric.scopeDimensions(lines[i][3][0],lines[i][3][1]),parametric.orderedPair(lines[i][4][0],lines[i][4][1]))
         for j in range(len(sub2)):
             curves.append(sub2[j])
+            curves[-1].vis = lines[i][-3]
             curves[-1].th = lines[i][-2]
             curves[-1].col = lines[i][-1]
     elif lines[i][0] == 8:
         sub = []
         for j in range(len(lines[i][3])):
             sub.append(auxCurves[lines[i][3][j]])
-        sub2 = parametric.doubleWarp(auxCurves[lines[i][1]],auxCurves[lines[i][2]],sub,parametric.scopeDimensions(lines[i][4][0],lines[i][4][1]),parametric.orderedPair(lines[i][5][0],lines[i][5][1]))
+        sub2 = parametric.doubleWarp(curves[lines[i][1]],curves[lines[i][2]],sub,parametric.scopeDimensions(lines[i][4][0],lines[i][4][1]),parametric.orderedPair(lines[i][5][0],lines[i][5][1]))
         for j in range(len(sub2)):
             curves.append(sub2[j])
+            curves[-1].vis = lines[i][-3]
             curves[-1].th = lines[i][-2]
             curves[-1].col = lines[i][-1]
     elif lines[i][0] == 9:
         sub = []
         for j in range(len(lines[i][5])):
-            sub.append(auxCurves[lines[i][5][j]])
-        sub2 = parametric.quadWarp(auxCurves[lines[i][1]],auxCurves[lines[i][2]],auxCurves[lines[i][3]],auxCurves[lines[i][4]],sub,parametric.scopeDimensions(lines[i][6][0],lines[i][6][1]),parametric.orderedPair(lines[i][7][0],lines[i][7][1]))
+            sub.append(curves[lines[i][5][j]])
+        sub2 = parametric.quadWarp(curves[lines[i][1]],curves[lines[i][2]],curves[lines[i][3]],curves[lines[i][4]],sub,parametric.scopeDimensions(lines[i][6][0],lines[i][6][1]),parametric.orderedPair(lines[i][7][0],lines[i][7][1]))
         for j in range(len(sub2)):
             curves.append(sub2[j])
+            curves[-1].vis = lines[i][-3]
             curves[-1].th = lines[i][-2]
             curves[-1].col = lines[i][-1]
     elif lines[i][0] == 10:
@@ -175,6 +131,7 @@ for i in range(len(lines)):
         sub2 = parametric.bSpline(parametric.orderedPair(lines[i][1][0],lines[i][1][1]),sub,parametric.orderedPair(lines[i][3][0],lines[i][3][1]),lines[i][4][0],lines[i][4][1])
         for j in range(len(sub2)):
             curves.append(sub2[j])
+            curves[-1].vis = lines[i][-3]
             curves[-1].th = lines[i][-2]
             curves[-1].col = lines[i][-1]
 
@@ -187,38 +144,75 @@ for i in range(len(shapes)):
     sub = parametric.prism(cl)
     if sub.isPrism:
         prisms.append(sub)
+        prisms[-1].col = shapes[i][1]
 
 # definition pixel matrix that represents each pixel in the image
 pixels = []
 for i in range(dimensions[1]):
     pixels.append([])
     for j in range(dimensions[0]):
-        pixels[i].append([bgCol[0],bgCol[1],bgCol[2]])
+        if form == ".png":
+            pixels[i].append([0,0,0,0])
+        else:
+            pixels[i].append([bgCol[0],bgCol[1],bgCol[2]])
 
-# draws all prisms
-for i in range(len(prisms)):
-    prismSpace = prisms[i].fill(pow(2,math.ceil(math.log2(max(dimensions[0],dimensions[1])))),1,True)
-    for j in range(len(prismSpace)):
-        for k in range(prismSpace[j].center.y,prismSpace[j].center.y+prismSpace[j].size+1):
-            for m in range(prismSpace[j].center.x,prismSpace[j].center.x+prismSpace[j].size+1):
-                for n in range(3):
-                    pixels[dimensions[1]-k-1][m][n] = shapes[i][1][n]
-
-# draws all curves, done second so that outlines are on top
-for i in range(len(curves)):
-    curvePixels = curves[i].generatePixels(curves[i].th)
+# draws a single curve
+def drawCurve(curve):
+    curvePixels = curve.generatePixels(curve.th)
     for j in range(len(curvePixels)):
         if curvePixels[j].pos.x >= 0 and curvePixels[j].pos.x < dimensions[0] and curvePixels[j].pos.y >= 0 and curvePixels[j].pos.y < dimensions[1]:
             newPixelColor = pixels[dimensions[1]-curvePixels[j].pos.y-1][curvePixels[j].pos.x]
-            for k in range(3):
-                newPixelColor[k] = int(newPixelColor[k]*curvePixels[j].magnitude+curves[i].col[k]*(1-curvePixels[j].magnitude))
+            if form == ".png":
+                sub = []
+                for k in range(3):
+                    sub.append(curve.col[k])
+                sub.append(1-curvePixels[j].magnitude)
+                newPixelColor = png.addColors(newPixelColor, sub)
+            else:
+                for k in range(3):
+                    newPixelColor[k] = int(newPixelColor[k]*curvePixels[j].magnitude+curve.col[k]*(1-curvePixels[j].magnitude))
             pixels[dimensions[1]-curvePixels[j].pos.y-1][curvePixels[j].pos.x] = newPixelColor
 
+# draws a single prism
+def drawPrism(prism):
+    prismSpace = prism.fill(pow(2,math.ceil(math.log2(max(dimensions[0],dimensions[1])))),1,True)
+    checked = []
+    for j in range(len(prismSpace)):
+        for k in range(prismSpace[j].center.y,prismSpace[j].center.y+prismSpace[j].size+1):
+            for m in range(prismSpace[j].center.x,prismSpace[j].center.x+prismSpace[j].size+1):
+                if form == ".png":
+                    if not [dimensions[1]-k-1,m] in checked:
+                        pixels[dimensions[1]-k-1][m] = png.addColors(pixels[dimensions[1]-k-1][m],prism.col)
+                        checked.append([dimensions[1]-k-1,m])
+                else:
+                    for n in range(3):
+                        pixels[dimensions[1]-k-1][m][n] = prism.col[n]
+
+# specifies a draw order for each curve/prism
+drawOrder = [
+    [0,1],
+
+    [0,0],
+    [1,0],
+    [2,0],
+
+    [1,1],
+
+    [3,0],
+    [4,0],
+    ]
+
+for i in range(len(drawOrder)):
+    if drawOrder[i][1]:
+        drawPrism(prisms[drawOrder[i][0]])
+    else:
+        drawCurve(curves[drawOrder[i][0]])
+
 # writing the final image file in selected format
-resFile = "result"
-form = ".bmp"
 if form == ".bmp":
     bitmap.image(pixels, resFile+form)
 elif form == ".jpg":
     jpg.image(pixels, resFile+form, 255)
+elif form == ".png":
+    png.image(pixels, resFile+form)
 
